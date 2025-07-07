e6d14035e3c14a583f57e8ed7190286e02384f79
Ulises Valero <ulises.valero@linksolution.local>
2025-07-07 13:52:46 -0300

    v.02 Second Commit

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const express = require('express')
 const fs = require('fs');
 
 class ProductManager {
@@ -28,13 +29,11 @@ class ProductManager {
     }
 
     getProducts() {
-        // en formato string
         const stringData = fs.readFileSync(this.path, 'utf8')
         console.log(stringData)
 
-        // en formato objeto
         const parsedData = JSON.parse(stringData)
-        console.log(parsedData)
+        return parsedData        
     }
 
     getProductsById(id) {
@@ -45,7 +44,7 @@ class ProductManager {
         let product = products.filter(p => p.id === id)
 
         if (product) {
-            console.log(product)
+            return product            
         }
         else {
             console.log(`producto con id ${id} no encontrado`)
@@ -55,48 +54,105 @@ class ProductManager {
 
 }
 
-class CartManager {
+    class CartManager {
 
-    constructor(route) {
-        this.path = route
-    }
-
-    addOrder() {
-        let stringData = fs.readFileSync(this.path, 'utf8')
-        let parsedData = JSON.parse(stringData)
-
-        if (!parsedData.order) parsedData.order = []
-        const order = parsedData.order
-        
-        let newOrder = {
-            id: order.length + 1,
-            products: []
+        constructor(route) {
+            this.path = route
         }
-        order.push(newOrder)
+
+        addOrder() {
+            let stringData = fs.readFileSync(this.path, 'utf8')
+            let parsedData = JSON.parse(stringData)
+
+            if (!parsedData.order) parsedData.order = []
+            const order = parsedData.order
+            
+            let newOrder = {
+                id: order.length + 1,
+                products: []
+            }
+            order.push(newOrder)
 
 
-        fs.writeFileSync(this.path, JSON.stringify(parsedData))
+            fs.writeFileSync(this.path, JSON.stringify(parsedData))
+        }
+
+        searchOrderById(id) {
+            let stringData = fs.readFileSync(this.path, 'utf8')
+            let parsedData = JSON.parse(stringData)
+
+            let idFiltrado = parsedData.order.find(o => o.id === id) 
+            console.log(idFiltrado)
+            return idFiltrado
+        }
+
+        deleteOrder() {
+
+        }
+
     }
 
-    searchOrder(id) {
-        let stringData = fs.readFileSync(this.path, 'utf8')
-        let parsedData = JSON.parse(stringData)
 
-        let idFiltrado = parsedData.order.find(o => o.id === id)
-        console.log(idFiltrado)
+    let ejemplo = new CartManager('./carts.json')
+    ejemplo.addOrder()
 
 
+    const app = express()
+    app.use(express.json())
+
+    const PORT = 8080
+
+    app.listen(PORT, () => {
+        console.log(`Servidor escuchando en el puerto ${PORT}`)
+    })
+
+
+    app.get('/carts/:cid', (req, res) => {
+        const cartOrder = new CartManager('./carts.json')
+        const filterOrder = cartOrder.searchOrderById(parseInt(req.params.cid))
+        try {  
+        res.status(200).json({
+            message: "Orden obtenida con éxito",
+            order: filterOrder
+        })
+        } catch (error) {
+        res.status(500).json({
+            message: "Error obteniendo la orden",
+            error: error.message
+        })
+    }
+    })  
+
+
+
+app.post('/carts', (req, res) => {
+    const cartOrder =  new CartManager('./carts.json')
+    const postOrder = cartOrder.addOrder()
+
+    try {
+        res.status(201).json({
+            message: "Orden agregada",
+            order: postOrder
+        })
+    } catch (error) {
+        res.status(500).json({
+            message: error.message,
+            error: "Error agregando la orden"
+        })
     }
 
-    deleteOrder() {
 
-    }
+})
 
-}
+app.post('/carts/:cid/product/:pid', (req, res) => {
+const cartOrder = new CartManager('./carts.json')
+const newProduct = new ProductManager('./products.json')
+const filteredOrder = cartOrder.searchOrderById(req.params.cid)
+const filteredProduct = newProduct.getProductsById(req.params.pid)
+const stringData = fs.readFileSync('./carts.json' , 'utf8')
+console.log(stringData)
+const parsedData = JSON.parse(stringData)
 
-let ejemplo1 = new CartManager('./carts.json')
-ejemplo1.addOrder("titulo")
 
-let ejemplo2 = new CartManager('./carts.json')
 
-ejemplo2.searchOrder(2)
+})
