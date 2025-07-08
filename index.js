9dbb13273f1733bdbb20f6d37a97dc8d6c15ab99
Ulises <u.ivalero@hotmail.com>
2025-07-08 11:19:31 -0300

    v.04 Primera entrega

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const express = require('express')
-const fs = require('fs');
+const fs = require('fs')
+const app = express()
+app.use(express.json())
+const PORT = 8080
+app.listen(PORT, () => {
+    console.log(`Servidor escuchando en el puerto ${PORT}`)
+})
 
 class ProductManager {
 
@@ -7,31 +13,34 @@ class ProductManager {
         this.path = route
     }
 
-    addProduct(title, description, price, thumbnail, code, stock) {
-        let stringData = fs.readFileSync(this.path, 'utf8')
-        let parsedData = JSON.parse(stringData)
 
-        if (!parsedData.products) parsedData.products = []
+    addProduct(product) {
+        const stringData = fs.readFileSync(this.path, 'utf8')
+        const parsedData = JSON.parse(stringData)
         const products = parsedData.products
 
-        const newProduct = {
+        let existingTitle = products.find(p => p.title === product.title)
+        if (existingTitle) throw new Error(`El producto con titulo ${product.title} ya existe`)
+
+        let existingCode = products.find(p => p.code === product.code)
+        if (existingCode) throw new Error(`El producto con el código ${product.code} ya existe`)
+
+        let newProduct = {
             id: products.length + 1,
-            title,
-            description,
-            price,
-            thumbnail,
-            code,
-            stock
+            ...product
         }
         products.push(newProduct)
 
-        fs.writeFileSync(this.path, JSON.stringify(parsedData))
+        let json = {
+            products: products,
+        }
+
+        fs.writeFileSync(this.path, JSON.stringify(json))
+        return json
     }
 
     getProducts() {
         const stringData = fs.readFileSync(this.path, 'utf8')
-        console.log(stringData)
-
         const parsedData = JSON.parse(stringData)
         return parsedData
     }
@@ -40,18 +49,134 @@ class ProductManager {
         const stringData = fs.readFileSync(this.path, 'utf8')
         const parsedData = JSON.parse(stringData)
 
+        if (!parsedData.products) parsedData.products = []
         let products = parsedData.products
+
         let product = products.find(p => p.id === id)
 
         if (product) {
             return product
         } else {
-            console.log(`Producto con id ${id} no encontrado`)
             return null
         }
     }
 
+    putProduct(pid, newData) {
+        const stringData = fs.readFileSync(this.path, 'utf8')
+        const parsedData = JSON.parse(stringData)
+        const products = parsedData.products
+
+        const index = products.find(p => p.id === pid)
+        if (!index) throw new Error(`Producto con id ${pid} no existe`)
+
+        products[index] = {
+            ...products[index],
+            ...newData,
+            id: pid
+        }
+
+        const updatedData = { products }
+
+        fs.writeFileSync(this.path, JSON.stringify(updatedData, null, 2))
+        return products[index]
+    }
+
+    deleteProduct(pid) {
+        const stringData = fs.readFileSync(this.path, 'utf8')
+        const parsedData = JSON.parse(stringData)
+        const products = parsedData.products
+
+        const filteredProduct = products.filter(p => p.id !== pid)
+
+        if (products.length == filteredProduct.length)
+            throw new Error(`El producto con el id ${pid} no existe`)
+
+        let json = {
+            products: filteredProduct
+        }
+
+        fs.writeFileSync(this.path, JSON.stringify(json))
+        return json
+    }
 }
+
+
+
+
+app.get('/products', (req, res) => {
+    try {
+        const newManager = new ProductManager('./products.json')
+        const newProduct = newManager.getProducts()
+        res.status(200).json({
+            message: "Productos obtenidos con éxito",
+            product: newProduct
+        })
+    } catch (error) {
+        throw new Error("No se pudieron obtener los productos")
+    }
+})
+
+app.get('/products/:pid', (req, res) => {
+    try {
+        const newManager = new ProductManager('./products.json')
+        const filteredProduct = newManager.getProductsById(parseInt(req.params.pid))
+        res.status(200).json({
+            message: "Producto filtrado obtenido",
+            products: filteredProduct
+        })
+    } catch (error) {
+        throw new Error(`No se pudo obtener el producto con ${pid}`)
+    }
+})
+
+app.post('/products', (req, res) => {
+    try {
+        const newManager = new ProductManager('./products.json')
+        let postProduct = req.body
+        res.status(200).json(
+            newManager.addProduct(postProduct),
+            { message: "Producto agregado con éxito", }
+        )
+    } catch (error) {
+        res.status(505).json({
+            message: error.message
+        })
+    }
+})
+
+app.delete('/products/:pid', (req, res) => {
+    try {
+        const newManager = new ProductManager('./products.json')
+        const pid = parseInt(req.params.pid)
+        const deleteProduct = newManager.deleteProduct(pid)
+        res.status(200).json({
+            message: "Producto eliminado con éxito",
+            products: deleteProduct
+        })
+    } catch (error) {
+        res.status(500).json({
+            message: error.message
+        })
+    }
+})
+
+app.put('/products/:pid', (req, res) => {
+    try {
+        const newManager = new ProductManager('./products.json')
+        const pid = parseInt(req.params.pid)
+        const putProduct = req.body
+        const updatedProduct = newManager.putProduct(pid, putProduct)
+        res.status(200).json({
+            message: "Producto modificado con éxito",
+            products: updatedProduct
+        })
+    } catch (error) {
+        res.status(500).json({
+            message: error.message
+        })
+    }
+})
+
 
 class CartManager {
 
@@ -80,7 +205,7 @@ class CartManager {
         const stringData = fs.readFileSync(this.path, 'utf8')
         const parsedData = JSON.parse(stringData)
 
-        const cart = parsedData.order.find(o => o.id === Number(cid))
+        const cart = parsedData.order.find(o => o.id === parseInt(cid))
         const productInCart = cart.products.find(p => p.id === pid)
 
         if (productInCart) {
@@ -91,7 +216,6 @@ class CartManager {
 
 
         fs.writeFileSync(this.path, JSON.stringify(parsedData))
-
     }
 
     searchOrderById(id) {
@@ -105,25 +229,10 @@ class CartManager {
 }
 
 
-
-
-
-
-
-const app = express()
-app.use(express.json())
-
-const PORT = 8080
-
-app.listen(PORT, () => {
-    console.log(`Servidor escuchando en el puerto ${PORT}`)
-})
-
-
 app.get('/carts/:cid', (req, res) => {
-    const cartOrder = new CartManager('./carts.json')
-    const filterOrder = cartOrder.searchOrderById(parseInt(req.params.cid))
     try {
+        const cartOrder = new CartManager('./carts.json')
+        const filterOrder = cartOrder.searchOrderById(parseInt(req.params.cid))
         res.status(200).json({
             message: "Orden obtenida con éxito",
             order: filterOrder
@@ -136,11 +245,10 @@ app.get('/carts/:cid', (req, res) => {
     }
 })
 
-
 app.post('/carts', (req, res) => {
-    const cartOrder = new CartManager('./carts.json')
-    const postOrder = cartOrder.addOrder()
     try {
+        const cartOrder = new CartManager('./carts.json')
+        const postOrder = cartOrder.addOrder()
         res.status(201).json({
             message: "Orden agregada",
             order: postOrder
@@ -154,37 +262,21 @@ app.post('/carts', (req, res) => {
 })
 
 app.post('/carts/:cid/products/:pid', (req, res) => {
-    const cartManager = new CartManager('./carts.json')
-    const productManager = new ProductManager('./products.json')
-
-    const cid = parseInt(req.params.cid)
-    const pid = parseInt(req.params.pid)
-
-    const cart = cartManager.searchOrderById(cid)
-    const product = productManager.getProductsById(pid)
-    console.log("El carrito:", cart)
-    console.log("El producto:", product)
-
-    const addProduct = cartManager.addProductToCart(cid, pid)
-
-    if (!addProduct) {
-        return res.status(500).json({ 
-            message: "No se agregó el producto al carrito" 
+    try {
+        const cartManager = new CartManager('./carts.json')
+        const productManager = new ProductManager('./products.json')
+        const cid = parseInt(req.params.cid)
+        const pid = parseInt(req.params.pid)
+        const cart = cartManager.searchOrderById(cid)
+        const product = productManager.getProductsById(pid)
+        cartManager.addProductToCart(cid, pid)
+        res.status(200).json({
+            message: `Al fin agregaste el producto: ${pid} al carrito id: ${cid}`,
+            carrito: cart
+        })
+    } catch (error) {
+        res.status(500).json({
+            message: error.message
         })
     }
-
-    res.status(200).json({
-        message: `Al fin agregaste el producto: ${pid} al carrito id: ${cid}`,
-        carrito: cart
-    })
-
-
 })
-
-
-
-
-// let ejemplo = new ProductManager('./products.json')
-
-// const llamada = ejemplo.getProductsById(2)
-// console.log(llamada)
